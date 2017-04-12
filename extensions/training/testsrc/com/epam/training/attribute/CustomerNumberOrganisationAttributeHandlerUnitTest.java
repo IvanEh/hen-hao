3333e1c136cf667734e8d2409adcc6d0666ce73c
Ivan Ehreshi <Ivan_Ehreshi@epam.com>
2017-04-12 14:38:15 +0300

    #9: Add tests for CustomerNumberOrganisationAttributeHandler

--- extensions/training/testsrc/com/epam/training/attribute/CustomerNumberOrganisationAttributeHandlerUnitTest.java
+++ extensions/training/testsrc/com/epam/training/attribute/CustomerNumberOrganisationAttributeHandlerUnitTest.java
@@ -1,8 +1,51 @@
 package com.epam.training.attribute;
 
+import com.epam.training.jalo.Organisation;
+import com.epam.training.model.OrganisationModel;
 import de.hybris.bootstrap.annotations.UnitTest;
+import de.hybris.platform.core.model.user.CustomerModel;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 @UnitTest
 public class CustomerNumberOrganisationAttributeHandlerUnitTest {
 
+    @Mock
+    OrganisationModel organisationModel;
+
+    CustomerNumberOrganisationAttributeHandler handler;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        handler = new CustomerNumberOrganisationAttributeHandler();
+    }
+
+    @Test
+    public void handlerGetWhenNoCustomers() throws Exception {
+        Mockito.when(organisationModel.getCustomers()).thenReturn(Collections.emptyList());
+
+        Assert.assertEquals(Integer.valueOf(0), handler.get(organisationModel));
+    }
+
+    @Test
+    public void handlerGetWhenTwoCustomers() throws Exception {
+        CustomerModel mockCustomer1 = Mockito.mock(CustomerModel.class);
+        CustomerModel mockCustomer2 = Mockito.mock(CustomerModel.class);
+
+        Mockito.when(organisationModel.getCustomers())
+                .thenReturn(Arrays.asList(mockCustomer1, mockCustomer2));
+
+        Assert.assertEquals(Integer.valueOf(2), handler.get(organisationModel));
+    }
 }
